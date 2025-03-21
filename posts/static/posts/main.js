0a3d67720ac731d6fe92a97dfb49fea8d321a3ce
sahiltalaviya <talaviyasahil5@gmail.com>
2025-03-21 17:18:42 -0400

    Learned how ajax call and DOM manipulation works in our project. we used simple hello world example to learn the concept, it will be removed later.

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,1 +1,15 @@
 console.log('Hello World')
+
+const helloworldbox = document.getElementById("Hello-World")
+
+$.ajax({
+    type: 'GET',
+    url: '/Hello-World/',
+    success: function(response){
+        console.log('success', response.text)
+        helloworldbox.textContent = response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import (
     post_list_and_create,
+    hello_world_view
 )
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
+
+    path('Hello-World/', hello_world_view, name='Hello-World')
 ]
