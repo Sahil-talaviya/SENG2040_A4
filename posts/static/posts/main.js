87d57923a5a92d5bd00d4b0b3275e12c57f3eca4
sahiltalaviya <talaviyasahil5@gmail.com>
2025-03-21 18:28:16 -0400

    Implemented Ajax to fetch and display posts dynamically, with a loading spinner. Created a custom JSON response in the view and updated main.js to render posts and control spinner visibility using setTimeout.

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,6 +1,8 @@
 console.log('Hello World')
 
 const helloworldbox = document.getElementById("Hello-World")
+const postsbox = document.getElementById("posts-box")
+const spinnerBox = document.getElementById('spinner-box')
 
 $.ajax({
     type: 'GET',
@@ -11,5 +13,27 @@ $.ajax({
     },
     error: function(error){
         console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        setTimeout(() => {
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(element => {
+                postsbox.innerHTML += `
+                    ${element.title} - <b>${element.body}</b> <br>
+                `
+            });
+        }, 100)
+
+    },
+    error: function(response){
+        console.log(error)
     }
 })
