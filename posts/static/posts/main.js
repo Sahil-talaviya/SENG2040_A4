2c4bb3a15a0748d600a876ac4ad0118e215569bd
sahiltalaviya <talaviyasahil5@gmail.com>
2025-04-02 19:42:42 -0400

    Modify the functionality of button, Previously, It was working from the django admin, now, we can like and unlike posts through buttons directly without even need of page to refresh. It also maintains counts.

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,9 +1,51 @@
 console.log('Hello World')
 
-const postsbox = document.getElementById("posts-box")
+const postsbox = document.getElementById('posts-box')
 const spinnerBox = document.getElementById('spinner-box')
 const loadBtn = document.getElementById('load-btn')
 const endBox = document.getElementById('end-box')
+
+const getCookie =(name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+const likeUnlikePosts = () => {
+    const likeUnlikeForms = [...document.getElementsByClassName('like-unlike-forms')]
+    likeUnlikeForms.forEach(form=> form.addEventListener('submit', e=>{
+        e.preventDefault()
+        const clickedId = e.target.getAttribute('data-form-id')
+        const clickedBtn = document.getElementById(`like-unlike-${clickedId}`)
+
+        $.ajax({
+            type: 'POST',
+            url: "/like-unlike/",
+            data: {
+                'csrfmiddlewaretoken': csrftoken,
+                'pk': clickedId,
+            },
+            success: function(response){
+                console.log(response)
+                clickedBtn.textContent = response.liked ? `Unlike (${response.count})`: `Like (${response.count})`
+            },
+            error: function(error){
+                console.log(error)
+            }
+        })
+    }))
+}
 
 let visible = 3
 
@@ -17,12 +59,12 @@ const getData = () => {
             setTimeout(() => {
                 spinnerBox.classList.add('not-visible')
                 console.log(data)
-                data.forEach(element => {
+                data.forEach(el => {
                     postsbox.innerHTML += `
                        <div class="card mb-2">
                             <div class="card-body">
-                                <h5 class="card-title">${element.title}</h5>
-                                <p class="card-text">${element.body}</p>
+                                <h5 class="card-title">${el.title}</h5>
+                                <p class="card-text">${el.body}</p>
                             </div>
                                 <div class="card-footer">
                                     <div class="row">
@@ -30,13 +72,16 @@ const getData = () => {
                                             <a href="#" class="btn btn-primary">Details</a>
                                         </div>
                                         <div class="col-2">
-                                            <a href="#" class="btn btn-primary">${element.liked ? `Unlike (${element.count})`: `Like (${element.count})`}</a>
+                                            <form class="like-unlike-forms" data-form-id="${el.id}">
+                                                <button href="#" class="btn btn-primary" id="like-unlike-${el.id}"> ${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
                         </div>
                     `
                 });
+                likeUnlikePosts()
             }, 100)
             console.log(response.size)
             if (response.size === 0) {
@@ -46,7 +91,7 @@ const getData = () => {
                 endBox.textContent = 'No more posts to load...'
             }
         },
-        error: function(response){
+        error: function(error){
             console.log(error)
         }
     })
