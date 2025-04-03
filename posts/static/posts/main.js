ec899b5e4e0bbc4625d8a196151d9fd5d223b393
sahiltalaviya <talaviyasahil5@gmail.com>
2025-04-03 17:27:50 -0400

    Resetted the form after adding new post through add post button. Moreover, Created Post details page after which will be shown on clicking details button. Also, added back, update and delete button on that page.

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -10,6 +10,8 @@ const title = document.getElementById('id_title')
 const body = document.getElementById('id_body')
 const csrf = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
+const url = window.location.href
+
 const alertBox = document.getElementById('alert-box')
 console.log('CSRF:', csrf);
 
@@ -77,7 +79,7 @@ const getData = () => {
                                 <div class="card-footer">
                                     <div class="row">
                                         <div class="col-2">
-                                            <a href="#" class="btn btn-primary">Details</a>
+                                            <a href="${url}${el.id}" class="btn btn-primary">Details</a>
                                         </div>
                                         <div class="col-2">
                                             <form class="like-unlike-forms" data-form-id="${el.id}">
@@ -147,6 +149,7 @@ postForm.addEventListener('submit', e=>{
                 likeUnlikePosts()
                 $('#addPostModal').modal('hide')
                 handleAlerts('success', 'New post Added!')
+                postForm.reset()
         },
         error: function(error){
             console.log(error)
