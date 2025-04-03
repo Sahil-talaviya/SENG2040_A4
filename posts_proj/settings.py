e0fa61c1049ac2eed5ca2c56d47942c5d14b69d1
sahiltalaviya <talaviyasahil5@gmail.com>
2025-04-02 20:32:34 -0400

    Created post creation form with validation. Added crispy forms styling and proper author assignment when saving posts.

--- posts_proj/settings.py
+++ posts_proj/settings.py
@@ -44,7 +44,10 @@ INSTALLED_APPS = [
 
     #3rd party apps
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
