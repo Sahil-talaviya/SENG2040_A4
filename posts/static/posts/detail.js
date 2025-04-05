0010735eff1cb6e53304af772d49e242af347590
sahiltalaviya <talaviyasahil5@gmail.com>
2025-04-05 18:47:58 -0400

    Done with the functionality of update and delete buttons. Used ajax to update post and delete post. Displayed alert boxes where relevant!

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -1,9 +1,21 @@
+console.log(window.location)
 const postBox = document.getElementById('post-box')
+
+const alertBox = document.getElementById('alert-box')
 const backBtn = document.getElementById('back-btn')
 const updateBtn = document.getElementById('update-btn')
 const deleteBtn = document.getElementById('delete-btn')
+
+
 const url = window.location.href + "data/"
+const updateUrl = window.location.href + "update/"
+const deleteUrl = window.location.href + "delete/"
+
+const updateForm = document.getElementById('update-form')
+const deleteForm = document.getElementById('delete-form')
+
 const spinnerBox = document.getElementById('spinner-box')
+const csrf = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
 const titleInput = document.getElementById('id_title')
 const bodyInput = document.getElementById('id_body')
@@ -30,9 +42,11 @@ $.ajax({
 
         const titleEl = document.createElement('h3')
         titleEl.setAttribute('class', 'mt-3')
+        titleEl.setAttribute('id', 'title')
 
         const bodyEl = document.createElement('p')
         bodyEl.setAttribute('class', 'mt-1')
+        bodyEl.setAttribute('id', 'body')
 
         titleEl.textContent = data.title
         bodyEl.textContent = data.body
@@ -48,4 +62,50 @@ $.ajax({
     error: function(error){
         console.log(error)
     }
+})
+
+updateForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    const title = document.getElementById('title')
+    const body = document.getElementById('body')
+
+    $.ajax({
+        type: 'POST',
+        url: updateUrl,
+        data: {
+            'csrfmiddlewaretoken': csrf,
+            'title': titleInput.value,
+            'body': bodyInput.value,
+        },
+        success: function(response){
+            console.log(response)
+            handleAlerts('success', 'post has been updated')
+            title.textContent = response.title
+            body.textContent = response.body
+        },
+        error: function(error){
+            console.log(error)
+        }
+        
+    })
+})
+
+deleteForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    $.ajax({
+        type: 'POST',
+        url: deleteUrl,
+        data: {
+            'csrfmiddlewaretoken': csrf,
+        },
+        success: function(response){
+            window.location.href = window.location.origin
+            localStorage.setItem('title', titleInput.value)
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })
 })
